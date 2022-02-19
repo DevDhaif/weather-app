230eb9dc84b138fec14cb4a42317be282388820f
DevDhaif <devdhaif@gmail.com>
2022-02-19 21:09:31 +0200

    fixed vid background dynamically  :heavy_check_mark:

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const api = {
 };
 function App() {
   const dateBuilder = (d) => {
-    let months = [
+    const months = [
       "January",
       "February",
       "March",
@@ -41,7 +41,7 @@ function App() {
 
   const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
-  const [bgv, setBgv] = useState("");
+  const [backgroundVideo, setBackgroundVideo] = useState("");
 
   const search = (e) => {
     if (e.key === "Enter") {
@@ -49,12 +49,11 @@ function App() {
         .then((res) => res.json())
         .then((result) => {
           setWeather(result);
-          setBgv(`${result.weather[0].description}`);
-
-          console.log(bgv);
+          setBackgroundVideo(`${result.weather[0].description}`);
           setQuery("");
+          // document.querySelector("#myvid > source").src = `/videos/${}`
+
         });
-      // console.log(result);
     }
   };
 
@@ -63,30 +62,52 @@ function App() {
       {/**Video Background */}
       {typeof weather.main !== "undefinde" &&
       typeof weather.sys !== "undefined" ? (
-
-      
-      <video
-        autoPlay
-        loop
-        muted
-        className="absolute  top-0 -z-10 opacity-90 left-0 w-full object-cover h-full filter blur-sm"
-      >
-        <source
-          src={`/videos/${weather.weather[0].description}.mp4`}
-          type="video/mp4"
-        />
-        Your browser does not support the video tag.
-      </video>
-      ):('')} 
+        <video 
+          id="myvid"
+          autoPlay
+          loop
+          muted
+          src={`/videos/${backgroundVideo}.mp4`}
+          className="absolute  top-0 -z-10 opacity-90 left-0 w-full object-cover h-full filter blur-sm"
+        >
+          
+          Your browser does not support the video tag.
+        </video>
+      ) : (
+        ""
+      )}
       <div className="w-full flex justify-center">
-        <input
-          className="block appearance-none opacity-80 shadow-lg hover:opacity-100 shadow-gray-300 px-4 py-3 mt-2 w-full mx-3 rounded-xl text-lg outline-none"
-          placeholder="search ..."
-          type="text"
-          onChange={(e) => setQuery(e.target.value)}
-          value={query}
-          onKeyPress={search}
-        />
+        <div className="flex mx-3 opacity-80 shadow-lg hover:opacity-100 mt-2">
+          <input
+            className=" appearance-none  shadow-gray-300 px-4 py-3  w-full  rounded-xl text-lg outline-none"
+            placeholder="search ..."
+            type="text"
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+          <button
+            className=" px-4 py-3  text-white   rounded-lg shadow-md  hover:shadow-lg   focus:shadow-lg focus:outline-none focus:ring-0 transition duration-150 ease-in-out  items-center"
+            type="button"
+            id="button-addon2"
+          >
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fas"
+              data-icon="search"
+              className="w-4"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path
+                fill="currentColor"
+                d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"
+              ></path>
+            </svg>
+          </button>
+        </div>
       </div>
       {typeof weather.main !== "undefinde" &&
       typeof weather.sys !== "undefined" ? (
--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react'
 
 export const Background = ({bgv}) => {
-    let vidUrl=`{/videos/${bgv}.mp4}`;
-    useEffect(()=>{
+    {/**
+        let vidUrl=`{/videos/${bgv}.mp4}`;
+        useEffect(()=>{
         vidUrl=`{/videos/${bgv}.mp4}`;
     },[bgv])
   return (
@@ -11,9 +12,10 @@ export const Background = ({bgv}) => {
         loop
         muted
         className="absolute  top-0 -z-10 opacity-90 left-0 w-full object-cover h-full filter blur-sm"
-      >
+        >
         <source src={vidUrl} type="video/mp4" />
         Your browser does not support the video tag.
-      </video>
-  )
+        </video>
+        )
+    */}
 }
