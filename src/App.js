0fe0792e1c18624d34dab780ac854b188a03b294
DevDhaif <devdhaif@gmail.com>
2022-02-22 00:31:53 +0200

    lil refactoring (last) :hammer:

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { Background } from "./components/Background";
-import { DropDown } from "./components/DropDown";
-import { Country, State, City }  from 'country-state-city';
+import { Country, State }  from 'country-state-city';
 import { dateBuilder } from "./DateBuilder";
 
 const apiUrl=process.env.REACT_APP_BASE_URL;
@@ -23,15 +22,11 @@ function App() {
   },[])
 
   function search (e) {
-    console.log(query);
-    
-    
       fetch(`${apiUrl}weather?q=${query}&units=metric&APPID=${apiKey}`)
         .then((res) => res.json())
         .then((result) => {
           setWeather(result);
           setBackgroundVideo(`${result.weather[0].description}`);
-          console.log(result);
           setQuery("");
 
         })
@@ -54,17 +49,16 @@ function App() {
 
   
   return (
-    <div className="bg-gray-900/40 min-h-screen p-4 xl:max-w-sm mx-auto">
-    
+    <div className="bg-gray-900/40 min-h-screen p-4 lg:max-w-4xl   mx-auto">
     
       {/**Video Background */}
       
         <Background backgroundVideo={backgroundVideo}/>
      
-      <div className="w-full flex justify-center">
-        <div className="flex mx-3 opacity-80 shadow-lg hover:opacity-100 mt-2">
+      <div className="w-full flex justify-center ">
+        <div className="flex mx-3 w-4/5 opacity-80 shadow-lg hover:opacity-100 mt-2">
           <input
-            className=" appearance-none  shadow-gray-300 px-4 py-3  w-full  rounded-xl rounded-tr-none rounded-br-none text-lg outline-none"
+            className=" appearance-none  shadow-gray-300 px-4 py-3  lg:w-full  rounded-xl rounded-tr-none rounded-br-none text-lg outline-none"
             placeholder="search ...(country,state or city)"
             type="text"
             onChange={(e) =>{ setQuery(e.target.value) 
@@ -75,39 +69,16 @@ function App() {
           <button className="text-xl text-white px-2 shadow-md rounded-tr-xl rounded-br-xl bg-gray-600 hover:shadow-lg   focus:shadow-lg focus:outline-none focus:ring-0 transition duration-150 ease-in-out  items-center" type="submit" onClick={handleClick}>
             Search
         </button>
-          {/**
-          <button
-            className=" px-4 py-3  text-white   rounded-lg "
-            type="button"
-            id="button-addon2"
-            onClick={handleClick}
-          >
-            <svg
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fas"
-              data-icon="search"
-              className="w-4"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-            >
-              <path
-                fill="currentColor"
-                d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"
-              ></path>
-            </svg>
-          </button>
-           */}
+          
           </div>
           </div>
       
           {/** Drop dwon for countries  */}
      
      
-          <div className='felx flex-col mt-2 space-y-4'>
-          <h1 className="text-xl text-center text-white">OR</h1>
-        <div id="countries" className="w-full flex justify-around">
+          <div className='felx flex-col mt-2 space-y-4 lg:flex lg:flex-row lg:flex-nowrap '>
+          <h1 className="text-xl text-center text-white lg:hidden">OR</h1>
+        <div id="countries" className="w-full flex justify-around lg:justify-center">
                 <label htmlFor="countries" className='text-white text-xl'>Country:</label>
             
                     <select className='w-44 ' name="countries" id="countries" onChange={handleChange}>
@@ -120,10 +91,10 @@ function App() {
                     
             </div>
 
-            <div className='w-full flex justify-around '>
+            <div className='w-full flex justify-around lg:justify-center '>
                 <label htmlFor="states" className="text-xl text-white">State:</label>
 
-                <select name="states" id="states" className='w-44 ml-6' onChange={(e)=>setQuery(`${e.target.value.replace(`'`,'').replace('Governorate','')},${query}`)}>
+                <select name="states" id="states" className='w-44 ml-6 lg:ml-0' onChange={(e)=>setQuery(`${e.target.value.replace(`'`,'').replace('Governorate','')},${query}`)}>
                         {states.map((state)=>(
                             
                     
@@ -140,21 +111,21 @@ function App() {
 
 
           {!weather.main && <h1 className="text-7xl text-white text-shadow mt-12">{errorMessage}</h1>}
-      {typeof weather.main !== "undefinde" &&
+      {typeof weather.main !== "undefined" &&
       typeof weather.sys !== "undefined" ? (
         <div>
           <div className="mt-6 ml-3 text-white text-center">
-            <h1 className="text-xl font-semibold   text-shadow">
+            <h1 className="text-xl lg:text-5xl my-5 font-semibold   text-shadow">
               {weather.name},{weather.sys.country}
             </h1>
-            <h2 className="font-semibold">{dateBuilder(new Date())}</h2>
+            <h2 className="font-semibold lg:text-2xl">{dateBuilder(new Date())}</h2>
           </div>
 
           <div className="mt-6 ml-3 space-y-4 flex flex-col justify-center items-center text-center text-white ">
-            <h2 className="text-7xl text-shadow font-bold mx-8 my-auto  bg-gray-900/20 rounded-xl px-4 py-8  shadow-lg shadow-gray-700/50 ">
+            <h2 className="text-7xl lg:text-9xl text-shadow font-bold mx-8 my-auto  bg-gray-900/20 rounded-xl px-4 py-8 lg:px-24 lg:py-28 shadow-lg shadow-gray-700/50 ">
               {Math.floor(weather.main.temp)} C
             </h2>
-            <img className="w-24 h-24 rounded-xl" src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}/>
+            <img className="w-24 h-24 rounded-xl" src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`} alt={weather.weather[0].description}/>
             <h2 className="text-5xl font-semibold">
             {weather.weather[0].description}
             </h2>
--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,35 +1,18 @@
-import React, { useEffect } from 'react'
 
 export const Background = ({backgroundVideo}) => {
-    {/**
-        let vidUrl=`{/videos/${bgv}.mp4}`;
-        useEffect(()=>{
-        vidUrl=`{/videos/${bgv}.mp4}`;
-    },[bgv])
-  return (
-    <video
-        autoPlay
-        loop
-        muted
-        className="absolute  top-0 -z-10 opacity-90 left-0 w-full object-cover h-full filter blur-sm"
-        >
-        <source src={vidUrl} type="video/mp4" />
-        Your browser does not support the video tag.
-        </video>
-        )
-    */}
+
     return(
 
         <video 
         id="myvid"
         autoPlay
         loop
-        muted
+        
         src={`/videos/${backgroundVideo}.mp4`}
         className="absolute  top-0 -z-10 opacity-90 left-0 w-full object-cover h-full filter blur-sm"
       >
         
-        Your browser does not support the video tag.
+        Your browser does not support the video tag
       </video>
       )
 }
